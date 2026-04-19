a552aef1cbf7b931b5f121479f06430d982e1081
faniatantya-gif <faniatantya@gmail.com>
2026-04-20 00:59:25 +0700

    second

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="E-Commerce Dashboard", layout="wide")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("main_data.csv")
+    df = pd.read_csv("dashboard/main_data.csv")
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     return df
 
